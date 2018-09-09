c0418d06420e7f20766b7c0d6a6c7f04cb885530
tomkent321 <tomkent321@yahoo.com>
2018-09-08 23:32:47 -0600

    cleaned up schema

--- schema.sql
+++ schema.sql
@@ -37,34 +37,11 @@ VALUES
 ("Bell Jet Ranger", "helicopater", 1750000, 6);
 
 
-
--- 1
-SELECT department_id, products.`department_name`,`over_head_costs`
-FROM departments
-INNER JOIN products ON departments.department_name = products.department_name GROUP BY department_id;
-
-SELECT department_id, products.`department_name`,`over_head_costs`
-FROM departments
-INNER JOIN products ON departments.department_name = products.department_name GROUP BY department_id;
-
-
-
-
-
+-- used in supervisor node
 SELECT departments.department_id, products.department_name, departments.over_head_costs, SUM(products.product_sales) AS dept_sales  
 FROM departments 
 INNER JOIN products ON departments.department_name = products.department_name GROUP BY department_id;
 
-
-var query = "SELECT department_name FROM departments";  //query without variable
-        connection.query(query, function(err, res) {
-
-          for(var i = 0; i < res.length; i++){
-            arrCurDept.push(res[i].department_name);
-          }
-          console.log(arrCurDept);
-          
-        }); 
 
 
 
